fa897071d0a12b1a297dff4fb6ac097762d7c467
99Kies <1290017556@qq.com>
2019-10-06 17:10:42 +0800

    update

--- sayhello/views.py
+++ sayhello/views.py
@@ -6,18 +6,12 @@ from sayhello.forms import HelloForm
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    # 加载所有记录
     messages = Message.query.order_by(Message.timestamp.desc()).all()
-    # 若不加最后的all() 返回的只是SQL语句, 加上all() 返回的就是 排好序的Message
-    # print(Message.query.order_by(Message.timestamp.desc()))
-    # print('\n')
-    # print(messages)
-    # 利用order_by()过滤器对数据库记录进行排序, 参数是排序的规则(即按发布的时间戳进行排序),排序方法为desc(),代表降序,asx()为..
     form = HelloForm()
     if form.validate_on_submit():
         name = form.name.data
         body = form.body.data
-        message = Message(name=name,body=body) # 实例化模型类, 创建记录(将提交表单的数据转移到数据库中)
+        message = Message(name=name,body=body)
         db.session.add(message)
         db.session.commit()
         flash('Your message have been sent to the world!')
